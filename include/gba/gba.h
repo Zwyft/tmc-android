2219d1d9752ce8f8b9ecdb231bf59e08590cdc04
Jak <jak@example.com>
2026-05-14 20:15:38 -0500

    fix: guard port_offset_USA.h include for ANDROID_PORT to avoid macro redefinition warnings

--- include/gba/gba.h
+++ include/gba/gba.h
@@ -7,7 +7,9 @@
 #include "macro.h"
 #include "multi_boot.h"
 #include "port/port_gba_mem.h"
+#ifndef ANDROID_PORT
 #include "port/port_offset_USA.h"
+#endif
 #include "syscall.h"
 #include "types.h"
 #include <string.h>
